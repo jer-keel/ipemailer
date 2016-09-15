4f98e107fb2a29539242b6d1194e39b6cd75ee33
jer-keel <jerkeeler@gmail.com>
2016-09-14 21:45:23 -0400

    Call ipemailer after 30 seconds

--- ipemailer.py
+++ ipemailer.py
@@ -1,24 +1,29 @@
 import ipgetter
 import json
 import smtplib
+from threading import Timer
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-config = json.load(open("email_config.json"))
-current_ip = ipgetter.myip()
+def get_and_send_ip():
+	config = json.load(open("email_config.json"))
+	current_ip = ipgetter.myip()
 
-# Create the messgae
-msg = MIMEMultipart()
-msg["From"] = config["from_addr"]
-msg["To"] = config["to_addr"]
-msg["Subject"] = "Server IP Address"
-body_text = "IP: " + current_ip
-body = MIMEText(body_text, "plain")
-msg.attach(body)
+	# Create the messgae
+	msg = MIMEMultipart()
+	msg["From"] = config["from_addr"]
+	msg["To"] = config["to_addr"]
+	msg["Subject"] = "Server IP Address"
+	body_text = "IP: " + current_ip
+	body = MIMEText(body_text, "plain")
+	msg.attach(body)
 
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login(config["from_addr"], config["from_pass"])
-text = msg.as_string()
-server.sendmail(config["from_addr"], config["to_addr"], text)
-server.quit()
+	server = smtplib.SMTP("smtp.gmail.com", 587)
+	server.starttls()
+	server.login(config["from_addr"], config["from_pass"])
+	text = msg.as_string()
+	server.sendmail(config["from_addr"], config["to_addr"], text)
+	server.quit()
+
+t = Timer(30.0, get_and_send_ip)
+t.start()
